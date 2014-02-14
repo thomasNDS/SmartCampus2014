358b1370fa7d2575488d22a5e3918e9fefd6d217
nicko <nicko466@gmail.com>
2014-02-14 16:29:54 +0100

    nothing to commit

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ SmartCampus2014
 ===============
 
 SmartCampus2014 project.
+
+Initialisation base de donnée
+==============================
+
+mongod --dbpath pathToDB 
+
+par défaut le chemin spécifié est /data/db
--- ServeurMqtt/HelloWorld.js
+++ ServeurMqtt/HelloWorld.js
@@ -7,6 +7,7 @@
 var http = require('http');
 var express = require('express');
 var mongoose = require('mongoose');
+var mqtt = require('mqtt')
 var Schema = mongoose.Schema;
 var restify = require('express-restify-mongoose')
 
@@ -15,33 +16,59 @@ var restify = require('express-restify-mongoose')
 //////////////////////////////////
 //Connection to the a mongodb database path localhost:port/nameOfCollection
 mongoose.connect("mongodb://localhost:27017/Client", function(err) {
-  if(err) { throw err; }
+    if (err) {
+        throw err;
+    }
 });
-
 
 //Definition of a schema
 var Temperature = new Schema({
-    _id : Number,
+    _id: Number,
     temp: Number
 });
 //Creation of the model
-var TemperatureModel = mongoose.model('temperature', Temperature);
+var TemperatureModel = mongoose.model('tables', Temperature);
 
 //Creation of an object which respect the model define before
-var myTemperatureModel = new TemperatureModel({ _id : 123, temp : 16});
+var myTemperatureModel = new TemperatureModel({_id: 1473, temp: 16});
 
 
 //saved the temperature in the mongodb database
-myTemperatureModel.save(function(err){
-      if (err) { throw err; }
-  console.log('Commentaire ajouté avec succès !');
-  // On se déconnecte de MongoDB maintenant
-  mongoose.connection.close();
+/*myTemperatureModel.save(function(err){
+ if (err) { throw err; }
+ console.log('Commentaire ajouté avec succès !');
+ // On se déconnecte de MongoDB maintenant
+ mongoose.connection.close();
+ });*/
+
+/*var query = TemperatureModel.findOne({_id: 123}, function(err, tempFind) {
+    if (err)
+        return handleError(err);
+    console.log('Temp find : %d', tempFind.temp)
+})*/
+
+///////////////////////////////////
+/////// MQTT  //////////////////
+//////////////////////////////////
+
+client = mqtt.createClient(1883, 'localhost');
+
+client.subscribe('temperature');
+var i = 12;
+client.on('message', function(topic, message) {
+    console.log(message);
+    var myTemperatureModel = new TemperatureModel({_id: i++, temp: message});
+    myTemperatureModel.save(function(err) {
+        if (err) {
+            throw err;
+        }
+        console.log('Température ajouté à la BD avec succès !');
+    });
 });
 
 
 var app = express();
-app.configure(function(){
+app.configure(function() {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     restify.serve(app, TemperatureModel);
