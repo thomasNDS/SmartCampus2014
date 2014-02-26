1793bb31a51167cae05220969f8ec7075209fcb8
thomas <nds.thomas@gmail.com>
2014-02-26 14:52:26 +0100

    README + api fonctionnelle

--- ServeurMqtt/README.md
+++ ServeurMqtt/README.md
@@ -4,10 +4,169 @@ MQQT server
 Install
 -----------
 
-sudo npm install mqtt express mongodb mongoose mongoose-schema-extend express-restify-mongoose 
+sudo npm install mqtt express mongodb mongoose mongoose-schema-extend express-restify-mongoose mers
 
 Launch
 -----------
 
 sudo service mongodb start
 nodejs serveur.js
+
+
+API
+=====
+
+you could then access it at
+    listing.
+    
+    http://localhost:4242/api/building/
+    http://localhost:4242/api/building/$id
+    http://localhost:4242/api/building/$id/items
+    http://localhost:4242/api/building/$id/items/$id
+    http://localhost:4242/api/building/$id/items/0
+    http://localhost:4242/api/building/finder/findTitleLike/term
+    
+    
+###Pagination
+Pagination is also supported via skip= and limit= query params.
+
+    http://localhost:4242/api/building/$id?skip=10&limit=10
+
+###Population
+Mongoose populate is supported, but this will be changing shortly to allow for more
+fine grained controll over population.  Currently you can do
+
+    http://localhost:4242/api/building?populate=items
+
+or to specify particular fields.
+
+    http://localhost:4242/api/building?skip=10&populate[items]=title,date
+
+
+
+###Filter
+Filtering is available for strings. To find all the blog posts with C in the title.
+
+    http://localhost:4242/api/building?filter[title]=C
+
+Also you can and or nor the filters by using + (and) - (nor)  or nothing or
+    http://localhost:4242/api/building?filter[-title]=C
+    http://localhost:4242/api/building?filter[+title]=C&filter[-body]=A
+
+
+
+To filter all String fields that have a C in them
+
+    http://localhost:4242/api/building?filter=C
+
+
+###Sorting
+Sorting is supported 1 ascending -1 ascending.
+
+  http://localhost:4242/api/building?sort=title:1,date:-1
+
+###Transformer
+Transformers can be registered on startup.  A simple TransformerFactory is
+included.  Something that takes security into account could be added.  Currently
+this is only supported on the get operations.   May change the responses to post
+to send location, though I find that pretty inconvient.
+
+
+```javascript
+
+app.use('/api', require('mers').rest({
+    mongoose:mongoose,
+    transformers:{
+           renameid:function(Model, label){
+            //do some setup but return function.
+              return function(obj){
+                obj.id = obj._id;
+                delete obj._id;
+                //don't forget to return the object.  Null will filter it from the results.
+                return obj;
+              }
+           }
+      }
+    }));
+}
+```
+
+to get results transformered just add
+
+     http://localhost:4242/api/building?transform=renameid
+
+
+
+It handles  get/put/post/delete I'll add some docs on that some day, but pretty much as you expect, or I expect anyways.
+see tests/routes-mocha.js for examples.
+
+###Static Finders
+It should also be able to be used with Class finders. Now handles class finders. Note: They must return  a query object.
+They are passed the query object and the rest of the url. All of the populate's, filters, transforms should work.
+
+```javascript
+
+/**
+ * Note this must return a query object.
+ * @param q
+ * @param term
+ */
+BlogPostSchema.statics.findTitleLike = function findTitleLike(q, term) {
+    return this.find({'title':new RegExp(q.title || term, 'i')});
+}
+
+```
+
+So you can get the url
+
+    http://localhost:4242/api/building/finder/findTitleLike?title=term
+
+or
+
+    http://localhost:4242/api/building/finder/findTitleLike/term
+
+### [Error Handling][error]
+To create a custom error handler
+
+```javascript
+
+   app.use('/rest, rest({
+         error : function(err, req, res, next){
+               res.send({
+                   status:1,
+                   error:err && err.message
+               });
+           }).rest());
+
+```
+
+### Custom Transformers
+You can transform your results by adding a custom transformer and or adding a new TransformerFactory
+
+```javascript
+
+   app.use('/rest, rest({
+         transformers :{
+          cooltranform:function(Model, label){
+             return function(obj){
+                    obj.id = obj._id;
+                    delete obj._id;
+                    return obj; //returning null removes it from the output
+             }
+          } }).rest());
+
+```
+
+### Selecting
+Selecting support is upcoming, but for now you can do it in finders
+
+```javascript
+ var User = new Schema({
+   username:String,
+   birthdate:Date
+ });
+ User.statics.selectJustIdAndUsername  = function(){
+  this.find({}).select('_id username');
+ }
+
+```
--- ServeurMqtt/serveur.js
+++ ServeurMqtt/serveur.js
@@ -2,17 +2,28 @@
  * SmartCampus
  */
 
-var http = require('http'),
+var addrmongo ='mongodb://localhost:27017/Client';
+
+var fs = require("fs");
+http = require('http'),
         express = require('express'),
         mongoose = require('mongoose'),
         extend = require('mongoose-schema-extend'),
         path = require("path"),
         mqtt = require('mqtt'),
         Schema = mongoose.Schema,
-        restify = require('express-restify-mongoose'),
-        routes = require('./routes');
+        restify = require('express-restify-mongoose');
 
 var app = express();
+var vm = require('vm');
+
+var includeInThisContext = function(path) {
+    var code = fs.readFileSync(path);
+    vm.runInThisContext(code, path);
+}.bind(this);
+var routes = require('./routes');
+
+includeInThisContext(__dirname + "/model.js");
 
 // Config
 app.configure(function() {
@@ -24,121 +35,16 @@ app.configure(function() {
 });
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
-//// Makes connection asynchronously.  Mongoose will queue up database
-//// operations and release them when the connection is complete.
-//mongoose.connect(uristring, function (err, res) {
-//  if (err) {
-//  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-//  } else {
-//  console.log ('Succeeded connected to: ' + uristring);
-//  }
-//});
 
 //////////////////////////////////
 /////// MONGODB  /////////////////
 //////////////////////////////////
 //Connection to the a mongodb database path localhost:port/nameOfCollection
-mongoose.connect("mongodb://localhost:27017/Client", function(err) {
+mongoose.connect(addrmongo, function(err) {
     if (err) {
         throw err;
     }
 });
-
-//////////////////////////////////
-//Definition of a schema /////////
-//////////////////////////////////
-var Building = new Schema({
-    name: {type: String, required: true},
-    comment: {type: String}
-});
-
-var Item = new Schema({
-    name: {type: String, required: true},
-    position: {type: String},
-    room_number: {type: Number}
-});
-
-var Sensors_data = new Schema({
-    type: {type: String},
-    position: {type: String}
-});
-
-var Mesure = new Schema({
-    date: {type: Date, default: Date.now},
-    value: String
-});
-
-var Comment = new Schema({
-    value: String
-});
-
-var Administrator = new Schema({
-    name: String,
-    first_name: String
-});
-
-var Entity = new Schema({
-    room_number: Number,
-    name: String,
-    first_name: String
-});
-
-var Tram = Entity.extend({
-    value: String
-});
-
-var Lampadaire = Entity.extend({
-    value: String
-});
-
-var Parking = Entity.extend({
-    total_place: Number,
-    vacant_place: Number
-});
-
-var Building_parking = Item.extend({
-    total_place: Number,
-    vacant_place: Number
-});
-
-var Cafeteria = Item.extend({
-    name: String
-});
-
-var Classroom = Item.extend({
-    total_place: Number
-});
-// models
-var ClassroomModel = mongoose.model('Classroom', Classroom);
-var CafeteriaModel = mongoose.model('Cafeteria', Cafeteria);
-var BuildingModel = mongoose.model('Building', Sensors_data);
-var ParkingModel = mongoose.model('Parking', Parking);
-var LampadaireModel = mongoose.model('Lampadaire', Lampadaire);
-var ItemModel = mongoose.model('Item', Item);
-var TramModel = mongoose.model('Tram', Tram);
-var EntityModel = mongoose.model('Entity', Entity);
-var AdministratorModel = mongoose.model('Administrator', Administrator);
-var CommentModel = mongoose.model('Comment', Comment);
-var MesureModel = mongoose.model('Mesure', Mesure);
-var Sensors_dataModel = mongoose.model('Sensors_data', Sensors_data);
-
-
-//Creation of an object which respect the model define before
-//var myBuildingModel = new BuildingModel({_id: 1473, temp: 16});
-
-//saved the Building in the mongodb database
-/*myBuildingModel.save(function(err){
- if (err) { throw err; }
- console.log('Commentaire ajouté avec succès !');
- // On se déconnecte de MongoDB maintenant
- mongoose.connection.close();
- });*/
-
-/*var query = BuildingModel.findOne({_id: 123}, function(err, tempFind) {
- if (err)
- return handleError(err);
- console.log('Temp find : %d', tempFind.temp)
- })*/
 
 ////////////////////////////////
 /////// MQTT  //////////////////
@@ -163,81 +69,10 @@ app.get('/api', function(req, res) {
     res.send('API is running');
 });
 
-app.get('/api/administrator', function(req, res) {
-    return AdministratorModel.find(function(err, Administrator) {
-        if (!err) {
-            return res.send(Administrator);
-        } else {
-            return console.log(err);
-        }
-    });
-});
-
-app.post('/api/administrator', function(req, res) {
-    var admin;
-    console.log("POST: ");
-    console.log(req.body);
-    admin = new AdministratorModel({
-        first_name: req.body.first_name,
-        name: req.body.name
-    });
-    admin.save(function(err) {
-        if (!err) {
-            return console.log("created");
-        } else {
-            return console.log(err);
-        }
-    });
-    return res.send(admin);
-});
-
-app.get('/api/administrator/:id', function(req, res) {
-    return AdministratorModel.findById(req.params.id, function(err, Administrator) {
-        if (!err) {
-            return res.send(Administrator);
-        } else {
-            return console.log(err);
-        }
-    });
-});
-
-app.put('/api/administrator/:id', function(req, res) {
-    return AdministratorModel.findById(req.params.id, function(err, administrator) {
-        administrator.first_name = req.body.first_name;
-        administrator.name = req.body.name;
-        return administrator.save(function(err) {
-            if (!err) {
-                console.log("updated");
-            } else {
-                console.log(err);
-            }
-            return res.send(administrator);
-        });
-    });
-});
-
-app.delete('/api/administrator/:id', function(req, res) {
-    return AdministratorModel.findById(req.params.id, function(err, administrator) {
-        return administrator.remove(function(err) {
-            if (!err) {
-                console.log("removed");
-                return res.send('');
-            } else {
-                console.log(err);
-            }
-        });
-    });
-});
-
 app.get('/', routes.index);
-//app.get('/add', routes.add);
-//app.get('/get', routes.get);
-//app.get('/delete', routes.delete);
-//app.get('/update', routes.update);
 
-//app.get(':name?', function hello(req, res, next) {
-//    res.render(req.params.name + 'html');
-//});
+var mers = require('mers');
+app.use('/api', mers({uri: addrmongo}).rest());
 
 //Run the server
 http.createServer(app).listen(4242, function() {
