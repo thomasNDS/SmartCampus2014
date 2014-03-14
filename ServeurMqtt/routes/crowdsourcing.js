c95e8a45e5e07cab119b12cda4e971012a4a48e7
simonplanes <simon.planes@laposte.net>
2014-03-14 17:57:32 +0100

    fonction de calcul des votes pour le crowdsourcing

--- ServeurMqtt/routes/crowdsourcing.js
+++ ServeurMqtt/routes/crowdsourcing.js
@@ -5,6 +5,8 @@
  */
 
 average = 5;
+var table_polls = [];
+maxTime = 1800; // time from which polls are ignored
 
 //Pour pouvoir faire une action sur le avg sans passer par une requete post (pour test)
 exports.voteRuGet = function(req, res) {
@@ -26,6 +28,34 @@ exports.getRu = function(req, res) {
     res.send("moyenne : " + average);
 };
 
-function processAvg(newVote){
-    average += newVote;
+function processAvg(newPoll) {
+    var mean = 0, coeffsSum = 0;
+    var currentTime = new Date();
+    table_polls.push({"date": new Date(), "poll": newPoll});
+    $.each(table_polls, function(index, value) {
+        var delay = (currentTime - value.date) / 1000;
+        currentCoeff = getCoeff(delay);
+        mean += value.poll * currentCoeff;
+        coeffsSum += currentCoeff;
+    });
+    average = mean / coeffsSum;
+    updateTable();
+}
+
+// delay: difference between the poll time and current time
+function getCoeff(delay) {
+    var res = maxTime - delay;
+    return (res >= 0 ? res : 0);
+}
+
+// remove polls that are too old
+function updateTable() {
+    var now = new Date();
+    while (table_polls.length > 0) {
+        if (table_polls[i].date < (now - maxTime)) {
+            table_polls.shift();
+        } else {
+            break;
+        }
+    }
 }
